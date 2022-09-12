a1bcdf9ddf2cb47908d0dc91a0c2b034b5210144
[Aurélien Bedouet] <[bedouet.aurelien@gmail.com]>
2022-09-12 15:51:24 +0200

    add logo header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import Link from "next/link";
+import { GiGolfFlag } from "react-icons/gi";
 
 const Header = () => (
   <div className="border-b mb-20 bg-white">
-    <div className="container mx-auto px-10 border-gray-300 py-6">
+    <div className="container mx-auto px-10 border-gray-300 py-4">
       <Link href="/">
-        <span className="cursor-pointer font-bold text-2xl sm:text-3xl text-gray-700">
-          AS Golf Olonnes
-        </span>
+        <div className="flex items-center gap-6">
+          <GiGolfFlag
+            size={48}
+            className="hidden sm:block text-gray-700 border border-green-400 rounded-full p-[5px] cursor-pointer"
+          />
+          <span className="cursor-pointer font-bold text-2xl sm:text-3xl text-gray-700">
+            AS Golf Olonnes
+          </span>
+        </div>
       </Link>
     </div>
   </div>
