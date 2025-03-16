1ba6df9dc94e2ed2a03ced7e18eee055d3035ec1
AurelienBedouet <bedouet.aurelien@gmail.com>
2025-03-16 16:34:35 +0100

    fixed a minor import bug

--- pages/articles/[slug].js
+++ pages/articles/[slug].js
@@ -1,7 +1,7 @@
 import React from "react";
 import {useRouter} from 'next/router';
 import {getPosts, getPostDetails} from "../../services";
-import Head from "next/head";
+import SEO from "../../components/SEO";
 import PostDetail from "../../components/PostDetail";
 import Categories from "../../components/Categories";
 import PostWidget from "../../components/PostWidget";
